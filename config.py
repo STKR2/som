75e1b51b2bb0554d783baa87f579231e302542c3
muntazer <102398991+muntazer995@users.noreply.github.com>
2022-06-13 16:32:37 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 from dotenv import load_dotenv
-from pyrogram import Client, filters
+from pyrogram import Client
 from pytgcalls import PyTgCalls
 
 # For Local Deploy
@@ -9,19 +9,12 @@ if os.path.exists(".env"):
     load_dotenv(".env")
 
 # Necessary Vars
-
-load_dotenv()
-admins = {}
-API_ID = int(os.getenv("API_ID", "8186557"))
-API_HASH = os.getenv("API_HASH", "efd77b34c69c164ce158037ff5a0d117")
-SESSION = os.getenv("SESSION", "AQC2O__E3DkkDBmp07B54NV86MGNCUq4NPUXHaaoZ86eTjQ1mir8cGAre6EUrdPWZfuwaURlyQwHJ3nfvrQlhs4fUogeK9dlIzTPTbs0tBEv3KJo5B2hbV--OXWTche8H6LDbyv_3QAcLxUhIVKcdGPl2ecpBGrfx4fR_0UXmUIw7yM58WxyzPlzDVorHyvAEpqln4tlnL-6dGZdgVjuDXm5r77IXFd_ridOfp4f9WDnlcRpvnC_60W6WqlC_g8SbAs_XYrGEbaxX0j-iIfW-IOl4RfQLhn0cS-E6p_tvtrsSCyUTNgdAlt2klDah61P9jnAAO2QzkfrXBKHCuOtlUkhdXuJZwA")
+API_ID = int(os.getenv("API_ID"))
+API_HASH = os.getenv("API_HASH")
+SESSION = os.getenv("SESSION")
 HNDLR = os.getenv("HNDLR", "/")
+SUDO_USERS = list(map(int, os.getenv("SUDO_USERS").split()))
 
-
-contact_filter = filters.create(
-    lambda _, __, message: (message.from_user and message.from_user.is_contact)
-    or message.outgoing
-)
-
+# Cellmusic
 bot = Client(SESSION, API_ID, API_HASH, plugins=dict(root="MusicAndVideo"))
 call_py = PyTgCalls(bot)
